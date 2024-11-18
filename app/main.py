73509096641489ca0ce71b214f4dad5da5c3cf3e
VictorGMendes <nonamebadass4@gmail.com>
2024-11-18 20:45:55 -0300

    alteracao main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "mundo melius quid fecisti"}
 
 @app.get("/items/{item_id}")
 def read_root(item_id: int, q: Union[str, None] = None):
